fa36343f150915f777ba382fb12a98615569698e
nadil11 <nadilnidhal111@gmail.com>
2024-08-07 19:35:56 +0530

    last commit

--- vite.config.js
+++ vite.config.js
@@ -5,19 +5,19 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    // proxy: {
-    //   '/api': {
-    //     target: 'https://verceltest-api.vercel.app',
-    //     changeOrigin: true,
-    //     rewrite: (path) => path.replace(/^\/api/, ''),
-    //   },
-    // },
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://verceltest-api.vercel.app',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://localhost:5000',
+    //     changeOrigin: true,
+    //     rewrite: (path) => path.replace(/^\/api/, ''),
+    //   },
+    // },
   }
 })
